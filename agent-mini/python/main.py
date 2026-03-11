35cf536ac620600d1b0333d5ff55c701cabd7a79
Rinat Abdullin <rinat@abdullin.com>
2026-03-11 16:53:36 +0100

    Tuning deps

--- agent-mini/python/main.py
+++ agent-mini/python/main.py
@@ -1,5 +1,15 @@
+from platform.platform_connect import PlatformServiceClientSync
+
+
 def main():
     print("Hello from python!")
+
+
+    client = PlatformServiceClientSync(address="http://127.0.0.1:8080")
+
+    client.health(HealthRequest())
+
+    vm = AgentMiniHarnessClientSync
 
 
 if __name__ == "__main__":
